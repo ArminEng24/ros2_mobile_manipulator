0f8abc7497026cb6242dd873d75c1e3300b1bdfc
Armin Mehrvar <armin.mehrvar@gmail.com>
2026-01-03 14:23:22 +0000

    modified paths to test urdf files

--- robot_description/launch/display.launch.py
+++ robot_description/launch/display.launch.py
@@ -8,10 +8,17 @@ import os
 
 def generate_launch_description():
     #  Define the path to the URDF file
+    # urdf_path = os.path.join(
+    #     get_package_share_path("robot_description"),
+    #     "urdf",
+    #     "mobile_manipulator.urdf.xacro",
+    # )
+
+    #  Define the path to the URDF file
     urdf_path = os.path.join(
         get_package_share_path("robot_description"),
         "urdf",
-        "mobile_manipulator.urdf.xacro",
+        "mobile_uvc.urdf.xacro",
     )
 
     # Define the path to the RViz configuration file
